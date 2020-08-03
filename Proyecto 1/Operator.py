9ce34f9a9f07171b24ba272b71f0cf5e327c5645
JavierRamirezCospin <ram18099@uvg.edu.gt>
2020-08-03 17:17:25 -0500

    Primer Commit
    - Logica inicial del programa

---
+++ Proyecto 1/Operator.py
@@ -0,0 +1,11 @@
+class Operator():
+    def __init__(self,joint,relation):
+        # joint = conjunto
+        self.joint = joint
+        # relation = relacion
+        self.relation = relation
+        self.isValid = True
+
+    # Funcion para primer parte
+
+    # Funcion para segunda parte 
