3f34031a102a1bd1837d8a0d2f1aa78b7d88be6e
JavierRamirezCospin <ram18099@uvg.edu.gt>
2020-08-03 17:30:01 -0500

    Validacion de Relacion sobre el conjunto

--- Proyecto 1/Operator.py
+++ Proyecto 1/Operator.py
@@ -5,7 +5,20 @@ class Operator():
         # relation = relacion
         self.relation = relation
         self.isValid = True
+        self.validateRelation(relation)
 
+    # PRUEBA PARA VER SI LA RELACION CONINCIDE CON EL CONJUNTO
+    def validateRelation(self,relation):
+        for relationship in relation:
+            num1 = relationship[0]
+            num2 = relationship[1]
+            if num1 not in self.joint:
+                self.isValid = False
+                print(str(num1) + " not in joint!")
+            elif num2 not in self.joint:
+                self.isValid = False
+                print(str(num2) + " not in joint!")
+                
     # Funcion para primer parte
 
     # Funcion para segunda parte 
